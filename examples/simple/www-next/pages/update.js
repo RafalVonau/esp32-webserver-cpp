33a6598c263116ec768a2fd473e3f32f296c5c3d
BubuHub <66834304+BubuHub@users.noreply.github.com>
2023-10-27 15:45:33 +0200

    Firmware update over www page is working :-)

--- examples/simple/www-next/pages/update.js
+++ examples/simple/www-next/pages/update.js
@@ -1,263 +1,272 @@
-import React, { useContext, useState } from 'react';
-import SwitchSidebar from '../lib/switchSidebar';
-import { Group, Text, useMantineTheme, MantineTheme, Title, Space, Button, Loader, Center, Progress, Modal } from '@mantine/core';
-import { Dropzone, DropzoneStatus, IMAGE_MIME_TYPE } from '@mantine/dropzone';
-import { Check, Upload, X, FilePlus, RefreshAlert } from 'tabler-icons-react';
-import { useModals } from '@mantine/modals';
-import { showNotification, updateNotification  } from '@mantine/notifications';
+import React, { useContext, useState } from "react";
+import SwitchSidebar from "../lib/switchSidebar";
+import {
+	Group,
+	Text,
+	useMantineTheme,
+	MantineTheme,
+	Title,
+	Space,
+	Button,
+	Loader,
+	Center,
+	Progress,
+	Modal,
+} from "@mantine/core";
+import { Dropzone, DropzoneStatus, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+import { Check, Upload, X, FilePlus, RefreshAlert } from "tabler-icons-react";
+import { useModals } from "@mantine/modals";
+import { showNotification, updateNotification } from "@mantine/notifications";
 import axios from "axios";
 
 function sleep(ms) {
-    return new Promise((resolve, reject) => {setTimeout(() => {resolve();}, ms);});
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve();
+		}, ms);
+	});
+}
+
+function toBuffer(ab) {
+    const buf = Buffer.alloc(ab.byteLength);
+    const view = new Uint8Array(ab);
+    for (let i = 0; i < buf.length; ++i) {
+        buf[i] = view[i];
+    }
+    return buf;
 }
 
 function Update() {
-    const theme = useMantineTheme();
-    const modals = useModals();
-    const [progress, setProgress] = useState(0);
-    const [progressModal, setProgressModal] = useState(false);
+	const theme = useMantineTheme();
+	const modals = useModals();
+	const [progress, setProgress] = useState(0);
+	const [progressModal, setProgressModal] = useState(false);
 
-    /*!
-     * \brief Use event source to monitor firmware update.
-     */
-    const monitorFirmwareUpdate = (formData) => {
-        return new Promise((resolve) => {
-            const es = new EventSource('/api/events');
-            let armed = 0;
-            es.onopen = function(e) { console.log("Events Opened"); };
-            es.onerror = function(e) {
-                if (e.target.readyState != EventSource.OPEN) {
-                    console.log("Events Closed");
-                    resolve('Firmware update failed');
-                }
-            };
-            es.addEventListener('cmd', function(r) {                
-                let j = JSON.parse(r.data);
-                console.log(j);
-                if ("state" in j) {
-                    if (j.state == "uploading") {
-                        setProgress(progress => j.progress);
-                        armed = 1;
-                    } else if (j.state == "idle") {
-                        es.close();
-                        if (armed) {                            
-                            resolve('');
-                        } else {
-                            resolve('The firmware update has not started!');
-                        }
-                    } else {  
-                        /* Ups - error */                          
-                        let error = j.message;
-                        if (error == '') {
-                            error = 'Firmware update failed';    
-                        }
-                        es.close();
-                        resolve(error);                    
-                    }
-                }
-            }, false);
-            axios.post('api/firmware', formData, {headers: {"Content-Type": "multipart/form-data",} }).then((res) => {
-                if (!res.data.ok) {
-                    es.close();
-                    resolve('Firmware update failed');
-                }
-            });
-        });
-    }
+	const wsFirmwareUpdate = (bb) => {
+		return new Promise(function (resolve, reject) {
+			try {
+				let binary = new Uint8Array(bb);
+				console.log(binary);
+				const ws = new WebSocket(`ws://${location.host}/ws`);
+				const id = Math.round(new Date().getTime() / 1000);
+				const maxDataSize = 4096;
+				const total = binary.length;
+				let pos = 0;
+				let uprogress = 0;
+				const cmd = `ota ${id} ${total}`;
 
-    /*!
-     * \brief Use Axjos and poll method (500ms intervwl) to monitor firmware update.
-     */
-    const monitorFirmwareUpdateAxjos = async (formData) => {
-        let armed = 0;
-        let error = '';
-        let res = await axios.post('api/firmware', formData, {headers: {"Content-Type": "multipart/form-data",} });
-        if (res.data.ok) {
-            while(true) {
-                await sleep(500);
-                let r = await axios.get('api/firmware')
-                console.log(r);
-                if (r.data.state == "uploading") {
-                    /* Uploading in progress... */
-                    setProgress(progress => r.data.progress);
-                    armed = 1;
-                } else if (r.data.state == "idle") {
-                    /* done :-) */
-                    if (armed) {
-                        error = '';
-                        break;
-                    } else {
-                        error = 'The firmware update has not started!';
-                        break;
-                    }
-                } else {  
-                    /* Ups - error */                          
-                    error = r.data.message;
-                    if (error == '') {
-                        error = 'Firmware update failed';    
-                    }
-                    break;
-                }
-            }
-        } else {
-            error = 'Firmware update failed';
-        }
-        return error;
-    }
+				console.log(`Firmware update total=${total}`);
 
+				let sendNextChunk = function () {
+					if (pos < total) {
+						let s = total - pos;
+						if (s > maxDataSize) s = maxDataSize;
+						const ab = new ArrayBuffer(s + 4);
+						/* Fill packet */
+						{
+							let view = new DataView(ab);
+							view.setUint32(0, id, true);
+							for (let i = 0; i < s; ++i) {
+								view.setUint8(4 + i, binary[pos + i]);
+							}
+						}
+						ws.send(toBuffer(ab));
+						pos += s;
+					} else {
+						// stop the progress bar
+						//if (!argv.np) bar1.stop();
+					}
+				};
 
-    const openConfirmUpdateModal = (files) => modals.openConfirmModal({
-        title: 'Do you really want to update the firmware?',
-        children: (
-            <Text size="sm">
-                Please confirm updating the firmware.
-            </Text>
-        ),
-        labels: { confirm: 'Update', cancel: 'Cancel' },
-        closeOnConfirm: false,
-        onCancel: () => console.log('Cancel'),
-        onConfirm: async () =>  {
-            modals.closeAll(); 
-            const file = files[0];
-            try {
-                const formData = new FormData();
-                formData.append("file", file);
-                let error = '';
-                setProgressModal(true);
+				ws.addEventListener("open", (event) => {
+					setProgress((progress) => uprogress);
+					console.time("ota");
+					ws.send(cmd);
+					sendNextChunk();
+				});
 
-                try {
-                    //error = await monitorFirmwareUpdateAxjos(formData);
-                    error = await monitorFirmwareUpdate(formData);
-                } catch (e) {
-                    error = 'Firmware update failed';
-                }
+				// Listen for messages
+				ws.addEventListener("message", (event) => {
+					let x = JSON.parse(event.data);
+					console.log(x);
+					if (x.cmd === "ota") {
+						if (x.ret != 0) {
+							console.log(`OTA upload error: ${x.ret}`);
+							resolve(-1);
+						}
+						uprogress = Math.round((100.0 * x.val) / total);
+						console.log(`Progress = ${uprogress} %`);
+						setProgress((progress) => uprogress);
+						if (x.val == total) {
+							console.timeEnd("ota");
+                            ws.close();
+							resolve(0);
+						} else {
+							sendNextChunk();
+						}
+					}
+				});
+                ws.addEventListener("error", (e) => { reject('WebSocket error');})
 
-                modals.closeAll(); 
-                setProgress(0);
-                setProgressModal(false);
-                if (error == '') {
-                    showNotification({
-                        autoClose: 5000,
-                        title: 'Firmware updated',
-                        message: ``,
-                        color: 'green',
-                        icon: <Check />,
-                    });
-                    modals.openConfirmModal({
-                        title: 'Do you want to restart the device?',
-                        children: (
-                            <Text size="sm">
-                                Please confirm device restart
-                            </Text>
-                        ),
-                        labels: { confirm: 'Restart', cancel: 'Cancel' },
-                        closeOnConfirm: false,
-                        onCancel: () => console.log('Cancel'),
-                        onConfirm: async () =>  {
-                            console.log('Restart');
-                            try {
-                                let r = await axios.get('api/action', {params: { action: 'restart'}})
-                            } catch (e) {
-                                console.log(e);
-                            }
-                            modals.closeAll(); 
-                            showNotification({
-                                id: 'restart',
-                                loading: true,
-                                autoClose: false,
-                                disallowClose: true,
-                                title: 'Restart',
-                                message: `The device is restarting...`,
-                                color: 'yellow',
-                                icon: <RefreshAlert />,
-                            });
-                            while(true) {
-                                /* Wait for operation completed. */
-                                await sleep(500);
-                                try {
-                                    const r = await axios.get('api/firmware',  {timeout: 500});
-                                    break;
-                                } catch(e) {
-                                    console.log(`Not yet...`);
-                                }
-                            }
-                            updateNotification({
-                                id: "restart",
-                                color: "yellow",
-                                title: "Restart",
-                                message: `Restart done :-)`,
-                                icon: <RefreshAlert />,
-                                autoClose: 4000,
-                                disallowClose: false,
-                            });
-                        }
-                    });
-                } else {
-                    showNotification({
-                        autoClose: 5000,
-                        title: 'Firmware update failed',
-                        message: error,
-                        color: 'red',
-                        icon: <X />,
-                    });
-                }
-            } catch (error) {
-                console.log(error);
-                modals.closeAll();
-                showNotification({
-                    autoClose: 5000,
-                    title: error.message,
-                    message: `Server response: ${error.response?.data?.error}`,
-                    color: 'red',
-                    icon: <X />,
-                });
-            }
-        }
-    });
+			} catch (e) {
+				console.error("UPSS", e);
+				reject(e);
+			}
+		});
+	};
 
-    return (
-        <SwitchSidebar pageContent={
-            <>
-            <Modal opened={progressModal} closeOnConfirm={false} withCloseButton={false} closeOnEscape={false} closeOnClickOutside={false} title= 'Update progress'>
-                <Progress value={progress} label={`${progress.toFixed(1)}%`} size="xl" radius="xl"/>
-            </Modal>
-                <Title order={1}>Firmware update</Title>
-                <Space h='sm' />
-                <Dropzone
-                    onDrop={(files) => openConfirmUpdateModal(files)}
-                    onReject={(files) => console.log('rejected files', files)}
-                >
-                    <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
-                        <Dropzone.Accept>
-                            <Upload
-                                size={50}
-                                color={theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6]}
-                            />
-                        </Dropzone.Accept>
-                        <Dropzone.Reject>
-                            <X
-                                size={50}
-                                color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
-                            />
-                        </Dropzone.Reject>
-                        <Dropzone.Idle>
-                            <FilePlus size={50} />
-                        </Dropzone.Idle>
+	const openConfirmUpdateModal = (files) =>
+		modals.openConfirmModal({
+			title: "Do you really want to update the firmware?",
+			children: <Text size="sm">Please confirm updating the firmware.</Text>,
+			labels: { confirm: "Update", cancel: "Cancel" },
+			closeOnConfirm: false,
+			onCancel: () => console.log("Cancel"),
+			onConfirm: async () => {
+				modals.closeAll();
+				const file = files[0];
+				let error = "";
+                console.log(file);
+				try {
+					setProgressModal(true);
 
-                        <div>
-                            <Text size="xl" inline>
-                                Drag files here or click to select files
-                            </Text>
-                            <Text size="sm" color="dimmed" inline mt={7}>
-                                Attach one file with the tar.gz extension
-                            </Text>
-                        </div>
-                    </Group>
-                </Dropzone>
-            </>
+					try {
+						const buf = await file.arrayBuffer();
+						error = await wsFirmwareUpdate(buf);
+					} catch (e) {
+						error = "Firmware update failed";
+					}
 
-        } />
-    );
+					modals.closeAll();
+					setProgress(0);
+					setProgressModal(false);
+					if (error == "") {
+						showNotification({
+							autoClose: 5000,
+							title: "Firmware updated",
+							message: ``,
+							color: "green",
+							icon: <Check />,
+						});
+						modals.closeAll();
+						showNotification({
+							id: "restart",
+							loading: true,
+							autoClose: false,
+							disallowClose: true,
+							title: "Restart",
+							message: `The device is restarting...`,
+							color: "yellow",
+							icon: <RefreshAlert />,
+						});
+						while (true) {
+							/* Wait for operation completed. */
+							await sleep(500);
+							try {
+	        					const r = await axios.get("api/info", { timeout: 500 });
+								break;
+							} catch (e) {
+								console.log(`Not yet...`);
+						    }
+						}
+						updateNotification({
+							id: "restart",
+							color: "yellow",
+							title: "Restart",
+							message: `Restart done :-)`,
+							icon: <RefreshAlert />,
+							autoClose: 4000,
+							disallowClose: false,
+						});
+					} else {
+						showNotification({
+							autoClose: 5000,
+							title: "Firmware update failed",
+							message: error,
+							color: "red",
+							icon: <X />,
+						});
+					}
+				} catch (error) {
+					console.log(error);
+					modals.closeAll();
+					setProgress(0);
+					setProgressModal(false);
+					showNotification({
+						autoClose: 5000,
+						title: error.message,
+						message: `Server response: ${error.response?.data?.error}`,
+						color: "red",
+						icon: <X />,
+					});
+				}
+			},
+		});
+
+	return (
+		<SwitchSidebar
+			pageContent={
+				<>
+					<Modal
+						opened={progressModal}
+						closeOnConfirm={false}
+						withCloseButton={false}
+						closeOnEscape={false}
+						closeOnClickOutside={false}
+						title="Update progress"
+					>
+						<Progress
+							value={progress}
+							label={`${progress.toFixed(1)}%`}
+							size="xl"
+							radius="xl"
+						/>
+					</Modal>
+					<Title order={1}>Firmware update</Title>
+					<Space h="sm" />
+					<Dropzone
+						onDrop={(files) => openConfirmUpdateModal(files)}
+						onReject={(files) => console.log("rejected files", files)}
+					>
+						<Group
+							position="center"
+							spacing="xl"
+							style={{ minHeight: 220, pointerEvents: "none" }}
+						>
+							<Dropzone.Accept>
+								<Upload
+									size={50}
+									color={
+										theme.colors[theme.primaryColor][
+											theme.colorScheme === "dark" ? 4 : 6
+										]
+									}
+								/>
+							</Dropzone.Accept>
+							<Dropzone.Reject>
+								<X
+									size={50}
+									color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
+								/>
+							</Dropzone.Reject>
+							<Dropzone.Idle>
+								<FilePlus size={50} />
+							</Dropzone.Idle>
+
+							<div>
+								<Text size="xl" inline>
+									Drag files here or click to select files
+								</Text>
+								<Text size="sm" color="dimmed" inline mt={7}>
+									Attach one file with the bin extension
+								</Text>
+							</div>
+						</Group>
+					</Dropzone>
+				</>
+			}
+		/>
+	);
 }
 
 export default Update;
